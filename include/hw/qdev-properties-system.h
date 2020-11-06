342f6aef18fcd64fd0b6a376d56ac9788fdb0efd
Eduardo Habkost <ehabkost@redhat.com>
2020-11-06 16:57:36 -0500

    fdc: DEFINE_PROP_FDC_DRIVE_TYPE macro
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- include/hw/qdev-properties-system.h
+++ include/hw/qdev-properties-system.h
@@ -75,5 +75,8 @@ extern const PropertyInfo qdev_prop_pcie_link_width;
 #define DEFINE_PROP_UUID_NODEFAULT(_name, _state, _field) \
     DEFINE_PROP(_name, _state, _field, qdev_prop_uuid, QemuUUID)
 
+#define DEFINE_PROP_FDC_DRIVE_TYPE(_name, _state, _field, _defval) \
+    DEFINE_PROP_SIGNED(_name, _state, _field, _defval,             \
+                       qdev_prop_fdc_drive_type, FloppyDriveType)
 
 #endif
