ed89a4f26e769ea578f2f207ef7477a1290e1d8e
Eduardo Habkost <ehabkost@redhat.com>
2020-11-27 14:33:02 -0500

    cpu: Move synchronize_from_tb() to tcg_ops
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -7258,7 +7258,7 @@ static void x86_cpu_common_class_init(ObjectClass *oc, void *data)
 #endif
     cc->dump_state = x86_cpu_dump_state;
     cc->set_pc = x86_cpu_set_pc;
-    cc->synchronize_from_tb = x86_cpu_synchronize_from_tb;
+    cc->tcg_ops.synchronize_from_tb = x86_cpu_synchronize_from_tb;
     cc->gdb_read_register = x86_cpu_gdb_read_register;
     cc->gdb_write_register = x86_cpu_gdb_write_register;
     cc->get_arch_id = x86_cpu_get_arch_id;
